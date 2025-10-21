c262125c71a174cf888f96454e0ba5e55cb77413
荀洪道 <kalxd@126.com>
2025-10-21 14:30:14 +0800

    简单写一写。

--- app/table.cc
+++ app/table.cc
@@ -1,6 +1,37 @@
 #include "table.h"
-#include <qabstractitemmodel.h>
 
 namespace XGApp {
 	TableModel::TableModel(QObject *parent) : QAbstractTableModel(parent) {}
+
+    int TableModel::rowCount(const QModelIndex &idx) const {
+        return this->colors.size();
+    }
+
+    int TableModel::columnCount(const QModelIndex &idx) const { return 7; }
+
+    QVariant TableModel::headerData(int section, Qt::Orientation orientation,
+                                    int role) const {
+
+		if (role != Qt::DisplayRole) {
+			return {};
+        }
+
+        if (section == 0) {
+			return "编号";
+        } else if (section == 1) {
+			return "名称";
+        } else if (section == 2) {
+			return "拼音";
+        } else if (section == 3) {
+			return "明亮亮的颜色";
+        } else if (section == 4) {
+			return "RGB";
+        } else if (section == 5) {
+			return "CMYK";
+        } else if (section == 6) {
+			return "HEX";
+        }
+
+        return {};
+    }
 }
--- app/table.h
+++ app/table.h
@@ -2,15 +2,19 @@
 #define XGAPP_TABLE
 
 #include <QAbstractTableModel>
+#include "lib.rs.h"
 
 namespace XGApp {
-	class TableCellData {
-	};
-
-
 	class TableModel : public QAbstractTableModel {
+    private:
+        rust::Vec<XGFFI::ColorItem> colors;
+
     public:
 		TableModel(QObject *parent = nullptr);
+
+        int rowCount(const QModelIndex &idx) const;
+        int columnCount(const QModelIndex &idx) const;
+        QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 	};
 
 	class Table {};
